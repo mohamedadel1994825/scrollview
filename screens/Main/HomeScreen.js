aa7f19bb193955bd62e91be886291dc9a696688a
Yahya <yaya1426@gmail.com>
2019-11-12 16:54:55 +0200

    top ,bottom and drawer navigation

--- screens/Main/HomeScreen.js
+++ screens/Main/HomeScreen.js
@@ -1,87 +1,50 @@
-import * as WebBrowser from 'expo-web-browser';
-import React from 'react';
+import * as WebBrowser from "expo-web-browser";
+import React from "react";
 import {
-	Image,
-	Platform,
-	StyleSheet,
-	ScrollView,
-	Text,
-	TouchableOpacity,
-	View,
-	Button,
-	SafeAreaView
-} from 'react-native';
-import HomeCharts from '../../components/Charts/HomeCharts';
-import { MonoText } from '../../components/StyledText';
-import appLogo from '../../assets/images/appLogo.png';
-import { Avatar } from 'react-native-elements';
+  Image,
+  Platform,
+  StyleSheet,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+  Button,
+  SafeAreaView
+} from "react-native";
+import HomeCharts from "../../components/Charts/HomeCharts";
+import { MonoText } from "../../components/StyledText";
+import appLogo from "../../assets/images/appLogo.png";
+import { Avatar } from "react-native-elements";
 // import { createDrawerNavigator } from 'react-navigation-drawer';
 // import { createAppContainer } from 'reaact-navigation';
 
 // createDrawerNavigator(RouteConfigs, DrawerNavigatorConfig);
-import { FontAwesome5 } from '@expo/vector-icons';
+import { FontAwesome5 } from "@expo/vector-icons";
 // import SafeAreaView from 'react-native-safe-area-view';
 // import { DrawerItems } from 'react-navigation-drawer';
 
-
-
 export default function HomeScreen() {
-	return (
-		<View>
-			{/* <SafeAreaView style={{ flex: 1 }}>
+  return (
+    <View>
+      {/* <SafeAreaView style={{ flex: 1 }}>
 				<TouchableOpacity style={{ margin: 15 }} onPress={() => this.props.navigation.openDrawer()}>
 					<FontAwesome5 name="bars" size={24} backgroundColor="red" />
 				</TouchableOpacity>
 			</SafeAreaView>
 		 */}
-			<HomeCharts />
-		</View>
-	);
+      <HomeCharts />
+    </View>
+  );
 }
 
 HomeScreen.navigationOptions = {
-	headerLeft: <View />,
-
-	headerTitle: (
-		<View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}>
-			<Image
-				source={require('../../assets/images/appLogo.png')}
-				style={{ width: 50, height: 50, justifyContent: 'center', alignItems: 'center' }}
-			/>
-		</View>
-	),
-	headerRight: (
-		<Avatar
-			size="small"
-			rounded
-			icon={{ name: 'user', type: 'font-awesome' }}
-			onPress={() => console.log('Works!')}
-			activeOpacity={0.7}
-			containerStyle={{ flex: 1, marginTop: 10, marginRight: 10 }}
-		/>
-	)
+  headerMode: "none"
 };
 
-
-
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
+  container: {
+    flex: 1,
 
-		backgroundColor: '#fff'
-	}
+    backgroundColor: "#fff"
+  }
 });
-
-
-
-
-
-// const DrawerNavigator = createDrawerNavigator({
-// 	HomeScreen,
-// 	ReportsScreen,
-// 	StockScreen,
-// 	UsersScreen,
-// 	SettingsScreen
-// });
-
-// HomeScreen.createAppContainer(DrawerNavigator);
